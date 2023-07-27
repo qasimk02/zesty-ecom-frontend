4ce22a1d778b63e6a4c2473a1c5bb75308dbdeb0
Mohammad Qasim <qassy02@gmail.com>
2023-07-27 20:32:49 +0530

    Desgined basic feature of Home page

---
+++ src/user/Data/HomeSectionData/MensShoes.js
@@ -0,0 +1,19 @@
+const mensShoes = [
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+  { imageUrl: "", brand: "", description: "" },
+];
+export default mensShoes;
---
+++ src/user/components/Carousel/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import HomeSectionCard from "../../Card/HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { Button } from "@mui/material";
+
+const HomeSectionCarousel = ({ sectionName, data }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const items = data.map((item) => <HomeSectionCard data={item} />);
+
+  const responsive = {
+    0: { items: 1.5 },
+    700: { items: 3 },
+    1080: { items: 4.5 },
+  };
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+  const syncActiveIndex = (items) => setActiveIndex(items);
+  console.log(activeIndex);
+  return (
+    <div className="border">
+      <h2 className="text-2xl font-extrabold text-gray-800 py-5 ml-3">
+        {sectionName}
+      </h2>
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          responsive={responsive}
+          disableButtonsControls
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        {/* left button */}
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "10rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(90deg)",
+              bgcolor: "white",
+              "&:hover": {
+                bgcolor: "white",
+              },
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(-90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+
+        {/* right button */}
+        {activeIndex !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "10rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+              "&:hover": {
+                bgcolor: "white",
+              },
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowRightIcon
+              sx={{ transform: "rotate(-90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarousel;
