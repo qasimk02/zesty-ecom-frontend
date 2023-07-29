74709d9b69d6deb4803da358726e8235b5a6e6e5
Mohammad Qasim <qassy02@gmail.com>
2023-07-29 20:07:56 +0530

    Designed Order and Checkout pages

---
+++ src/user/components/Card/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className="space-y-2">
+        <p className="font-semibold">Mohammad Qasim</p>
+        <p>Cst Road, Kurla, Mumbai, 400070</p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>9695339121</p>
+        </div>
+        <hr></hr>
+      </div>
+    </div>
+  );
+};
+export default AddressCard;
